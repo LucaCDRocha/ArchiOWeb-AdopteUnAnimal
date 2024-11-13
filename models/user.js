b8b3cf2aa6722426b0c9998ae276768cdd5a97aa
LucaCDRocha <lucadrocha@gmail.com>
2024-11-13 14:52:01 +0100

    Refactor user model schema to include additional fields and rename for clarity

--- models/user.js
+++ models/user.js
@@ -1,12 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const utilisateurSchema = new Schema({
-  nom: String,
-  prenom: String,
-  email: String
+const userSchema = new Schema({
+	firstName: String,
+	lastName: String,
+	email: String,
+	password: String,
+	likes: [{ type: mongoose.Types.ObjectId, ref: "Pet" }],
+	dislikes: [{ type: mongoose.Types.ObjectId, ref: "Pet" }],
+	adoptions: [{ type: mongoose.Types.ObjectId, ref: "Adoption" }],
 });
 
-const Utilisateur = mongoose.model('Utilisateur', utilisateurSchema);
+const User = mongoose.model("User", userSchema);
 
-export default Utilisateur;
+export default User;
