80865923a1301297575043f3d767d2c31632603d
LucaCDRocha <lucadrocha@gmail.com>
2024-11-13 15:28:04 +0100

    Add GET route for retrieving pet by ID

--- routes/pets.js
+++ routes/pets.js
@@ -3,7 +3,11 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("Got a response from the pets route");
+	res.send("Got a response from the pets route");
+});
+
+router.get("/:id", function (req, res, next) {
+	res.send("Got a response from the pets route with id");
 });
 
 export default router;
