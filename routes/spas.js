85e149314c3fa647d5b2f65244e1e5fb4d18ad82
Robin Frossard <frossard.robin@hotmail.com>
2024-11-13 15:38:53 +0100

    add routes for spa

--- routes/spas.js
+++ routes/spas.js
@@ -6,4 +6,24 @@ router.get("/", function (req, res, next) {
   res.send("Got a response from the spas route");
 });
 
+router.get("/:id", function (req, res, next) {
+  res.send("Got a response from the spa route with id");
+});
+
+router.post("/", function (req, res, next) {
+  res.send("Got a response from the spa route post");
+});
+
+router.get("/:id/pets", function (req, res, next) {
+  res.send("Got a response from the spa route with all the pets");
+});
+
+router.put("/:id", function (req, res, next) {
+  res.send("Got a response from the spa route to modify the spa");
+});
+
+router.delete("/:id", function (req, res, next) {
+  res.send("Got a response from the spa route to delete the spa");
+});
+
 export default router;
