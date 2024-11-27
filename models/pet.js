73e62d3d941e1d48c51a50ac9bfb721b45110575
Elodie Perring <elodie.perring@heig-vd-ch>
2024-11-27 13:42:36 +0100

    Add image support to pet model

--- models/pet.js
+++ models/pet.js
@@ -2,12 +2,18 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const petSchema = new Schema({
-	nom: String,
-	description: String,
-	tags: [{ type: mongoose.Types.ObjectId, ref: "Tag" }],
-	spa_id: { type: mongoose.Types.ObjectId, ref: "Spa" },
-	likes_count: Number,
-	dislikes_count: Number,
+  nom: String,
+  description: String,
+  images: [
+    {
+      data: Buffer,
+      contentType: String,
+    },
+  ],
+  tags: [{ type: mongoose.Types.ObjectId, ref: "Tag" }],
+  spa_id: { type: mongoose.Types.ObjectId, ref: "Spa" },
+  likes_count: Number,
+  dislikes_count: Number,
 });
 
 const Pet = mongoose.model("Pet", petSchema);
