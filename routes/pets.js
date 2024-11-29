ad8a096ce315b833512b95c99466619a64433292
Elodie Perring <elodie.perring@heig-vd-ch>
2024-11-29 13:26:39 +0100

    Enhance pets route with improved error handling and response messages

--- routes/pets.js
+++ routes/pets.js
@@ -4,47 +4,108 @@ import Pet from "../models/pet.js";
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-	Pet.find()
-		.sort("dislikes_count")
-		.exec()
-		.then((pets) => {
-			res.send(pets);
-		})
-		.catch((err) => {
-			next(err);
-		});
+  Pet.find()
+    .sort("dislikes_count")
+    .exec()
+    .then((pets) => {
+      res.send(pets);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.get("/:id", function (req, res, next) {
-	res.send("Got a response from the pets route with id");
+  Pet.findById(req.params.id)
+    .exec()
+    .then((pet) => {
+      if (!pet) {
+        return res.status(404).send("Pet not found");
+      }
+      res.send(pet);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.post("/", function (req, res, next) {
-	res.send("Got a POST request from the pets route");
+  const newPet = new Pet(req.body);
+  newPet
+    .save()
+    .then((pet) => {
+      res.status(201).send(pet);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
-
 router.patch("/:id", function (req, res, next) {
-	res.send("Got a PATCH request from the pets route with id");
+  Pet.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .exec()
+    .then((pet) => {
+      if (!pet) {
+        return res.status(404).send("Pet not found");
+      }
+      res.send(pet);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.delete("/:id", function (req, res, next) {
-	res.send("Got a DELETE request from the pets route with id");
+  Pet.findByIdAndDelete(req.params.id)
+    .exec()
+    .then((pet) => {
+      if (!pet) {
+        return res.status(404).send("Pet not found");
+      }
+      res.send("Pet deleted successfully");
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.put("/:id/like", function (req, res, next) {
-	res.send("Got a PUT request from the pets route with id/like");
+  Pet.findByIdAndUpdate(
+    req.params.id,
+    { $inc: { likes_count: 1 } },
+    { new: true }
+  )
+    .exec()
+    .then((pet) => {
+      if (!pet) {
+        return res.status(404).send("Pet not found");
+      }
+      res.send(pet);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.put("/:id/dislike", function (req, res, next) {
-	res.send("Got a PUT request from the pets route with id/dislike");
-});
-
-router.delete("/:id/like", function (req, res, next) {
-	res.send("Got a DELETE request from the pets route with id/like");
+  Pet.findByIdAndUpdate(
+    req.params.id,
+    { $inc: { dislikes_count: 1 } },
+    { new: true }
+  )
+    .exec()
+    .then((pet) => {
+      if (!pet) {
+        return res.status(404).send("Pet not found");
+      }
+      res.send(pet);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.delete("/:id/dislike", function (req, res, next) {
-	res.send("Got a DELETE request from the pets route with id/dislike");
+  res.send("Got a DELETE request from the pets route with id/dislike");
 });
 
 export default router;
