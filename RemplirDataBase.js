7e9617ce84749e7246b8b267c2c2f7d4c6525e11
LucaCDRocha <lucadrocha@gmail.com>
2024-12-04 15:20:49 +0100

    Refactor database seeder to use bcrypt for password hashing and update database connection configuration

--- RemplirDataBase.js
+++ RemplirDataBase.js
@@ -1,39 +1,39 @@
 // Importer les dépendances nécessaires
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import Pet from "./models/pet.js";
 import Spa from "./models/spa.js";
 import User from "./models/user.js";
 import Tag from "./models/tag.js";
 import Adoption from "./models/adoption.js";
-import fs from 'fs';
-import path from 'path';
-
-dotenv.config();
+import bcrypt from "bcrypt";
+import * as config from "./config.js";
+import fs from "fs";
+import path from "path";
 
 // Connexion à la base de données
-mongoose.connect(
-  process.env.DATABASE_URL || "mongodb://localhost/ArchiOWeb-AdopteUnAnimal"
-);
-const userParis = new User({
-  firstName: "Jane",
-  lastName: "Doe",
-  email: "jane.doe@example.com",
-  password: "password123",
-});
-const userclient = new User({
-  firstName: "John",
-  lastName: "Doe",
-  email: "john.doe@example.com",
-  password: "password123",
+mongoose.connect(config.databaseUrl);
 
+const hashedPasswordParis = await bcrypt.hash("password123", config.bcryptCostFactor);
+const userParis = new User({
+	firstName: "Jane",
+	lastName: "Doe",
+	email: "jane.doe@example.com",
+	password: hashedPasswordParis,
+});
+
+const hashedPasswordClient = await bcrypt.hash("password123", config.bcryptCostFactor);
+const userclient = new User({
+	firstName: "John",
+	lastName: "Doe",
+	email: "john.doe@example.com",
+	password: hashedPasswordClient,
 });
 const spaParis = new Spa({
-  nom: "SPA de Paris",
+	nom: "SPA de Paris",
 	adresse: "Paris",
 	latitude: 567,
 	longitude: 1235,
-  user_id: userParis._id,
+	user_id: userParis._id,
 });
 
 const spaMorges = new Spa({
@@ -51,167 +51,166 @@ const spaZurich = new Spa({
 });
 
 const tagChien = new Tag({
-  nom: "Chien",
+	nom: "Chien",
 });
 
-
-const imageMorges = fs.readFileSync('Images/spaMorges_chien.jpg');
-const imageParis = fs.readFileSync('Images/spaParis_chien.jpg');
-const imageZurich = fs.readFileSync('Images/spaZurich_chien.jpg');
-
+const imageMorges = fs.readFileSync("Images/spaMorges_chien.jpg");
+const imageParis = fs.readFileSync("Images/spaParis_chien.jpg");
+const imageZurich = fs.readFileSync("Images/spaZurich_chien.jpg");
 
 const chienToffee = new Pet({
-  nom: "Toffee",
-  age:12,
-  description: "Un chien très mignon qui a 12 ans bientôt 13 ans",
-  images: [
-    {
-      data: imageParis,
-      imgType: "image/jpeg",
-    },
-  ],
-  tags: [tagChien._id],
-  spa_id: spaParis._id,
-  likes_count: 0,
-  dislikes_count: 0,
+	nom: "Toffee",
+	age: 12,
+	description: "Un chien très mignon qui a 12 ans bientôt 13 ans",
+	images: [
+		{
+			data: imageParis,
+			imgType: "image/jpeg",
+		},
+	],
+	tags: [tagChien._id],
+	spa_id: spaParis._id,
+	likes_count: 0,
+	dislikes_count: 0,
 });
 const chienCara = new Pet({
-  nom: "Cara",
-  age:12,
-  description: "Un chien très mignon qui a 12 ans bientôt 13 ans",
-  images: [
-    {
-      data: imageParis,
-      imgType: "image/jpeg",
-    },
-  ],
-  tags: [tagChien._id],
-  spa_id: spaParis._id,
-  likes_count: 0,
-  dislikes_count: 0,
+	nom: "Cara",
+	age: 12,
+	description: "Un chien très mignon qui a 12 ans bientôt 13 ans",
+	images: [
+		{
+			data: imageParis,
+			imgType: "image/jpeg",
+		},
+	],
+	tags: [tagChien._id],
+	spa_id: spaParis._id,
+	likes_count: 0,
+	dislikes_count: 0,
 });
 const chienBeth = new Pet({
-  nom: "Bethany",
-  age:10,
-  description: "Un chien très mignon qui a 10 ans. Très calme et affectueuse, adore jouer avec les enfants",
-  images: [
-    {
-      data: imageParis,
-      imgType: "image/jpeg",
-    },
-  ],
-  tags: [tagChien._id],
-  spa_id: spaParis._id,
-  likes_count: 0,
-  dislikes_count: 0,
+	nom: "Bethany",
+	age: 10,
+	description: "Un chien très mignon qui a 10 ans. Très calme et affectueuse, adore jouer avec les enfants",
+	images: [
+		{
+			data: imageParis,
+			imgType: "image/jpeg",
+		},
+	],
+	tags: [tagChien._id],
+	spa_id: spaParis._id,
+	likes_count: 0,
+	dislikes_count: 0,
 });
 const chienArthur = new Pet({
-  nom: "Arthur",
-  age: 3,
-  description: "Un chien très mignon qui a 3ans. Border Collie croisé avec golden retriever", 
-  images: [
-    {
-      data: imageMorges,
-      imgType: "image/jpeg",
-    },
-  ],
-  tags: [tagChien._id],
-  spa_id: spaMorges._id,
-  likes_count: 1,
-  dislikes_count: 0,
+	nom: "Arthur",
+	age: 3,
+	description: "Un chien très mignon qui a 3ans. Border Collie croisé avec golden retriever",
+	images: [
+		{
+			data: imageMorges,
+			imgType: "image/jpeg",
+		},
+	],
+	tags: [tagChien._id],
+	spa_id: spaMorges._id,
+	likes_count: 1,
+	dislikes_count: 0,
 });
 const chienMerlan = new Pet({
-  nom: "Merlan",
-  age: 3,
-  description: "Un chien très mignon qui a 3ans. Border Collie croisé avec golden retriever", 
-  images: [
-    {
-      data: imageMorges,
-      imgType: "image/jpeg",
-    },
-  ],
-  tags: [tagChien._id],
-  spa_id: spaMorges._id,
-  likes_count: 0,
-  dislikes_count: 0,
+	nom: "Merlan",
+	age: 3,
+	description: "Un chien très mignon qui a 3ans. Border Collie croisé avec golden retriever",
+	images: [
+		{
+			data: imageMorges,
+			imgType: "image/jpeg",
+		},
+	],
+	tags: [tagChien._id],
+	spa_id: spaMorges._id,
+	likes_count: 0,
+	dislikes_count: 0,
 });
 const chienCookie = new Pet({
-  nom: "Cookie",
-  age: 1,
-  description: "Un chiot de tout juste une année qui veut être aimée et être caressée.", 
-  images: [
-    {
-      data: imageMorges,
-      imgType: "image/jpeg",
-    },
-  ],
-  tags: [tagChien._id],
-  spa_id: spaMorges._id,
-  likes_count: 0,
-  dislikes_count: 0,
+	nom: "Cookie",
+	age: 1,
+	description: "Un chiot de tout juste une année qui veut être aimée et être caressée.",
+	images: [
+		{
+			data: imageMorges,
+			imgType: "image/jpeg",
+		},
+	],
+	tags: [tagChien._id],
+	spa_id: spaMorges._id,
+	likes_count: 0,
+	dislikes_count: 0,
 });
 const chienBat = new Pet({
-  nom: "Bat",
-  age: 3,
-  description: "Un chiot de tout juste 3 ans qui veut être aimée et être caressée.", 
-  images: [
-    {
-      data: imageZurich,
-      imgType: "image/jpeg",
-    },
-  ],
-  tags: [tagChien._id],
-  spa_id: spaZurich._id,
-  likes_count: 0,
-  dislikes_count: 0,
+	nom: "Bat",
+	age: 3,
+	description: "Un chiot de tout juste 3 ans qui veut être aimée et être caressée.",
+	images: [
+		{
+			data: imageZurich,
+			imgType: "image/jpeg",
+		},
+	],
+	tags: [tagChien._id],
+	spa_id: spaZurich._id,
+	likes_count: 0,
+	dislikes_count: 0,
 });
 const chienJoie = new Pet({
-  nom: "Joie",
-  age: 1,
-  description: "Un chiot de tout juste une année qui veut être aimée et être caressée.", 
-  images: [
-    {
-      data: imageZurich,
-      imgType: "image/jpeg",
-    },
-  ],
-  tags: [tagChien._id],
-  spa_id: spaZurich._id,
-  likes_count: 0,
-  dislikes_count: 0,
+	nom: "Joie",
+	age: 1,
+	description: "Un chiot de tout juste une année qui veut être aimée et être caressée.",
+	images: [
+		{
+			data: imageZurich,
+			imgType: "image/jpeg",
+		},
+	],
+	tags: [tagChien._id],
+	spa_id: spaZurich._id,
+	likes_count: 0,
+	dislikes_count: 0,
 });
 const chienCaline = new Pet({
-  nom: "Caline",
-  age: 1,
-  description: "Caline est un chiot d'un ans qui adore les câlins et les balades. Elle est très affectueuse et aime jouer avec les enfants.", 
-  images: [
-    {
-      data: imageZurich,
-      imgType: "image/jpeg",
-    },
-  ],
-  tags: [tagChien._id],
-  spa_id: spaZurich._id,
-  likes_count: 1,
-  dislikes_count: 0,
+	nom: "Caline",
+	age: 1,
+	description:
+		"Caline est un chiot d'un ans qui adore les câlins et les balades. Elle est très affectueuse et aime jouer avec les enfants.",
+	images: [
+		{
+			data: imageZurich,
+			imgType: "image/jpeg",
+		},
+	],
+	tags: [tagChien._id],
+	spa_id: spaZurich._id,
+	likes_count: 1,
+	dislikes_count: 0,
 });
 
 const adoption = new Adoption({
-  date: new Date(),
-  user_id: userclient._id,
-  pet_id: chienArthur._id,
-  messages: [
-    {
-      content: "Je suis intéressé par l'adoption de ce chien.",
-      date: new Date(),
-      user_id: userclient._id,
-    },
-    {
-      content: "Merci pour votre intérêt. Nous allons examiner votre demande.",
-      date: new Date(),
-      user_id: userParis._id,
-    },
-  ],
+	date: new Date(),
+	user_id: userclient._id,
+	pet_id: chienArthur._id,
+	messages: [
+		{
+			content: "Je suis intéressé par l'adoption de ce chien.",
+			date: new Date(),
+			user_id: userclient._id,
+		},
+		{
+			content: "Merci pour votre intérêt. Nous allons examiner votre demande.",
+			date: new Date(),
+			user_id: userParis._id,
+		},
+	],
 });
 userclient.likes = [chienArthur._id];
 // Supprimer toutes les collections existantes
@@ -232,6 +231,10 @@ chienBeth.save();
 chienCara.save();
 adoption.save();
 
-
-chienToffee.save().then(() => console.log("Fini")).catch((error) => console.error("Erreur", error));
-
+chienToffee
+	.save()
+	.then(() => {
+		console.log("Fini");
+		mongoose.connection.close();
+	})
+	.catch((error) => console.error("Erreur", error));
