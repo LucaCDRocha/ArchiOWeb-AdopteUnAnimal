81e6f60be83336c288d0c83cea8391454f26265c
Robin Frossard <frossard.robin@hotmail.com>
2024-12-04 15:50:27 +0100

    Users Post Test

---
+++ spec/users.spec.js
@@ -0,0 +1,20 @@
+import supertest from "supertest"
+import app from "../app.js"
+
+describe('POST /users', function () {
+    it('should create a user', async function () {
+        const res = await supertest(app)
+            .post('/users')
+            .send({
+                name: 'John Doe',
+                password: '1234'
+            })
+            .expect(200)
+        .expect('Content-Type', /json/);
+    });
+});
+
+
+describe('GET /users', function () {
+    test.todo('should retrieve the list of users');
+});
