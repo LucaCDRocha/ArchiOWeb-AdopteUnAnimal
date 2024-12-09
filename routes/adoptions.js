576ae55957f18a36f87aa241a24e42ca08a24c27
lucasHeig <lucas.tschaler@heig-vd.ch>
2024-12-09 14:20:45 +0100

    fix route adoptions

--- routes/adoptions.js
+++ routes/adoptions.js
@@ -7,7 +7,7 @@ import { authenticate } from "./auth.js";
 const router = express.Router();
 
 router.get("/", authenticate, function (req, res, next) {
-  Adoption.findOne({ user_id: req.currentUserId})
+  Adoption.find({ user_id: req.currentUserId})
     .populate("pet_id")
     .exec()
     .then((adoptions) => {
